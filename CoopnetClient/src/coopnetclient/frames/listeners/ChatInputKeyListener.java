3f58a211fe90d951338dc2c97a98e5765b5b6381
kecske85 <kecske85@f6eb1f37-6750-0410-a7d3-e344b5e70ba5>
2009-03-11 19:28:28 +0000

    some basic spam prevention: long messages and rapid messages

--- CoopnetClient/src/coopnetclient/frames/listeners/ChatInputKeyListener.java
+++ CoopnetClient/src/coopnetclient/frames/listeners/ChatInputKeyListener.java
@@ -24,27 +24,32 @@ import coopnetclient.protocol.out.Protocol;
 import coopnetclient.enums.ChatStyles;
 import coopnetclient.frames.FrameOrganizer;
 import coopnetclient.frames.clientframetabs.TabOrganizer;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JTextPane;
 
 public class ChatInputKeyListener implements KeyListener {
 
-    public final static int CHANNEL_CHAT_MODE = 0;
-    public final static int ROOM_CHAT_MODE = 1;
-    public final static int PRIVATE_CHAT_MODE = 2;
+    public static final int CHANNEL_CHAT_MODE = 0;
+    public static final int ROOM_CHAT_MODE = 1;
+    public static final int PRIVATE_CHAT_MODE = 3;
+    public static final int MIN_MESSAGE_INTERVAL = 800; //ms
+    public static final int MAX_ALLOWED_RAPID_MESSAGES = 3;
+    public static final int[] MAXIMUM_MESSAGE_LENGTHS = {1000, 2500, 2500};
+    public static final String MSG_TOO_LONG_CHANNEL = "Long messages not allowed in channels!";
+    public static final String MSG_TOO_LONG_OTHER = "Could not send message, because it is too big!";
+    public static final String WHISPER_SELF = "Private chat with yourself is not allowed!";
+    public static final String SPAM_WARNING = "SPAM WARNING! Send messages slower!";
     private boolean ctrlIsPressed;
-    public int mode;
+    private int mode;
     private String prefix;
+    private long lastMessageDate;
+    private int spamCount;
 
-    /**
-     * modes:   <br>
-     * 0 - main     <br>
-     * 1 - room<    br>
-     * 2 - private  <br>
-     */
     public ChatInputKeyListener(int mode, String prefix) {
         this.prefix = prefix; // players name in private chat or channel ID
         this.mode = mode;
+        lastMessageDate = System.currentTimeMillis();
     }
 
     public void setPrefix(String prefix) {
@@ -58,74 +63,101 @@ public class ChatInputKeyListener implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         JTextPane source = (JTextPane) e.getSource();
-        if (e.getKeyCode() == KeyEvent.VK_CONTROL) {
-            ctrlIsPressed = true;
-        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_CONTROL:
+                ctrlIsPressed = true;
+                break;
+            case KeyEvent.VK_ENTER:
+                if (ctrlIsPressed) { //CTRL+ENTER prints new line
+                    if (source.getText().length() > 0 
+                            && !source.getText().startsWith("\n")
+                            && !source.getText().endsWith("\n\n")) {
+                        source.setText(source.getText() + "\n");
+                    }
+                } else {
+                    String command = source.getText();
 
-            if (ctrlIsPressed) { //CTRL+ENTER prints new line
-                if (source.getText().length() > 0 && !source.getText().startsWith("\n") && !source.getText().endsWith("\n\n")) {
-                    source.setText(source.getText() + "\n");
-                }
-            } else {
+                    //dont send anything if empty
+                    if ((command.trim()).equals("")) {
+                        return;
+                    }
+                    //ANTI_SPAM:
+                    //A - prevent spamming by disallowing rapid messages
+                    long date = System.currentTimeMillis();
+                    if ((date - lastMessageDate) < MIN_MESSAGE_INTERVAL) {
+                        lastMessageDate = date;
+                        spamCount++;
+                        if (spamCount > MAX_ALLOWED_RAPID_MESSAGES) {
+                            FrameOrganizer.getClientFrame().printSystemMessage(SPAM_WARNING, false);
+                            source.setText("");
+                            return;
+                        }
+                    } else {
+                        spamCount = 0;
+                        lastMessageDate = date;
+                    }
 
-                String command = source.getText();
+                    //B - TODO if message is "long" check agains a pattern to see
+                    //if its the same text repeated many times -> spam
 
-                //dont send anything if empty
-                if (command.equals("")) {
-                    return;
-                }
-                if ((command.trim()).equals("")) {
-                    return;
-                }
-                if (command.length() > 2500) {
-                    FrameOrganizer.getClientFrame().printSystemMessage("Could not send message, because it is too big!", false);
-                    source.setText("");
-                    return;
-                }
-
-                //whisper command
-                if (command.startsWith("/w ")) {
-                    String tail = command.substring(3);
-                    int nameEnd = tail.indexOf(" ");
-                    if (nameEnd == -1) {
-                        FrameOrganizer.getClientFrame().printSystemMessage("Usage: /w username message", false);
+                    //check length
+                    if (command.length() > MAXIMUM_MESSAGE_LENGTHS[mode]) {
+                        if (mode == CHANNEL_CHAT_MODE) {
+                            FrameOrganizer.getClientFrame().printSystemMessage(MSG_TOO_LONG_CHANNEL, false);
+                        } else {
+                            FrameOrganizer.getClientFrame().printSystemMessage(MSG_TOO_LONG_OTHER, false);
+                        }
                         source.setText("");
                         return;
                     }
-                    String name = tail.substring(0, nameEnd);
-                    String msg = tail.substring(nameEnd + 1, tail.length());
-                    if (name.equals(Globals.getThisPlayerLoginName())) {
-                        FrameOrganizer.getClientFrame().printSystemMessage("Private chat with yourself is not allowed!", false);
+
+                    //whisper command
+                    if (command.startsWith("/w ")) {
+                        String tail = command.substring(3);
+                        int nameEnd = tail.indexOf(" ");
+                        if (nameEnd == -1) {
+                            FrameOrganizer.getClientFrame().printSystemMessage("Usage: /w username message", false);
+                            source.setText("");
+                            return;
+                        }
+                        String name = tail.substring(0, nameEnd);
+                        String msg = tail.substring(nameEnd + 1, tail.length());
+                        if (name.equals(Globals.getThisPlayerLoginName())) {
+                            FrameOrganizer.getClientFrame().printSystemMessage(WHISPER_SELF, false);
+                            source.setText("");
+                            return;
+                        }
+                        Protocol.privateChat(name, msg);
                         source.setText("");
                         return;
                     }
-                    Protocol.privateChat(name, msg);
-                    source.setText("");
-                    return;
-                }
-
-                if (command.startsWith("/invite ")) {
-                    String name = command.substring(8);
-                    Protocol.sendRoomInvite(name);
-                    source.setText("");
-                    return;
-                }
-
-                if (this.mode == CHANNEL_CHAT_MODE) { //main chat
-                    Protocol.mainChat(prefix, command);
-                } else if (this.mode == ROOM_CHAT_MODE) { //room chat
-                    Protocol.roomChat(command);
-                } else if (this.mode == PRIVATE_CHAT_MODE) { // private chat
-
-                    PrivateChatPanel privatechat = TabOrganizer.getPrivateChatPanel(prefix);
-
-                    if (privatechat != null) {
-                        privatechat.append(Globals.getThisPlayerLoginName(), command, ChatStyles.WHISPER);
+                    //invite command
+                    if (command.startsWith("/invite ")) {
+                        String name = command.substring(8);
+                        Protocol.sendRoomInvite(name);
+                        source.setText("");
+                        return;
                     }
-                    Protocol.privateChat(prefix, command);
+
+                    //chat command
+                    if (this.mode == CHANNEL_CHAT_MODE) { //main chat
+                        Protocol.mainChat(prefix, command);
+                    } else if (this.mode == ROOM_CHAT_MODE) { //room chat
+                        Protocol.roomChat(command);
+                    } else if (this.mode == PRIVATE_CHAT_MODE) { // private chat
+
+                        PrivateChatPanel privatechat = TabOrganizer.getPrivateChatPanel(prefix);
+
+                        if (privatechat != null) {
+                            privatechat.append(Globals.getThisPlayerLoginName(), command, ChatStyles.WHISPER);
+                        }
+                        Protocol.privateChat(prefix, command);
+                    }
+                    source.setText("");
                 }
-                source.setText("");
-            }
+                break;
+            default:
+                break;
         }
     }
 
