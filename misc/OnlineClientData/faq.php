49fd6b6f7eb62dd92594adbd84035c208dffbc2f
Edwin Stang <gsubes@gmail.com>
2009-07-12 16:44:44 +0000

    onlineclientdata and sourceforge class commiting, not yet implemented, project.xmls updated coz of netbeans 6.7

---
+++ misc/OnlineClientData/faq.php
@@ -0,0 +1,10 @@
+<?php
+header("Location: http://coopnet.wiki.sourceforge.net/faq");
+exit; ?>
+<html>
+<head>
+</head>
+<body>
+If you have not been automatically redirected, you should click <a href="http://coopnet.wiki.sourceforge.net/faq">here</a>.
+</body>
+</html>
---
+++ misc/OnlineClientData/latestUpdater.php
@@ -0,0 +1,1 @@
+<?php header("Location: http://dfn.dl.sourceforge.net/sourceforge/coopnet/CoopnetUpdater-4.jar");exit; ?>
