57501653ea887317115f0277a0cf605d12397359
Edwin Stang <gsubes@gmail.com>
2009-10-06 21:48:38 +0000

    Leere Logmeldung

--- misc/OnlineClientData/LatestClient.php
+++
@@ -1,1 +0,0 @@
-<?php header("Location:http://dfn.dl.sourceforge.net/sourceforge/coopnet/coopnetclient-0.102.1.zip");exit; ?>
--- misc/OnlineClientData/LatestUpdater.php
+++
@@ -1,1 +0,0 @@
-<?php header("Location: http://dfn.dl.sourceforge.net/sourceforge/coopnet/CoopnetUpdater-4.jar");exit; ?>
